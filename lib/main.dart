a0e608d8e2bc45283257722bfb8fe83fb0b35ccc
xakimov04 <mardonhakimov0404@gmail.com>
2024-06-11 20:18:09 +0500

    mvvm arxitekturasi yozildi

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:tech_shop/home_page.dart';
 
 void main(List<String> args) {
   runApp(const MyApp());
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: Scaffold(),
     );
   }
 }
