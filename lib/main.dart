5846b118317f660121aa6c81499152f138715a69
abdujalilabdulaxatov <abdujalilabdulaxattov895@gmail.com>
2024-06-11 19:59:01 +0500

    tea=st

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,10 @@ void main(List<String> args) {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      // Abdujalil
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
