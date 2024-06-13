77a4d22dd24cee6f8835b424cb6e6e8a75de16b4
ibragimov05 <ibragimov.asus15@gmail.com>
2024-06-13 19:41:27 +0500

    home screendagi hato to'girlandi

--- lib/views/screens/home_screen/views/home_screen.dart
+++ lib/views/screens/home_screen/views/home_screen.dart
@@ -1,3 +1,88 @@
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:tech_shop/viewmodels/home_view_model.dart';
+// import 'package:tech_shop/views/screens/home_screen/widgets/carousel_widget.dart';
+// import 'package:tech_shop/views/screens/home_screen/widgets/category_widget.dart';
+// import 'package:tech_shop/views/screens/home_screen/widgets/product_widget.dart';
+//
+// class HomeScreen extends StatelessWidget {
+//   final HomeViewModel viewModel = HomeViewModel();
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return DefaultTabController(
+//       length: 2,
+//       child: Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Colors.white,
+//           surfaceTintColor: Colors.white,
+//           title: const TextField(
+//             decoration: InputDecoration(
+//               filled: true,
+//               fillColor: Color(0xFFF3F4F8),
+//               label: Text(
+//                 "Mahsulotlar va turkumlar qidirish",
+//                 style: TextStyle(
+//                   color: Color(0xff8B8B95),
+//                 ),
+//               ),
+//               border: InputBorder.none,
+//               contentPadding:
+//                   EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+//             ),
+//           ),
+//           actions: [
+//             IconButton(
+//               onPressed: () {},
+//               icon: const Icon(
+//                 CupertinoIcons.heart,
+//                 size: 29,
+//               ),
+//             ),
+//           ],
+//         ),
+//         body: ListView(
+//           children: [
+//             Expanded(
+//               child: Column(
+//                 children: [
+//                   Carousel(viewModel: viewModel),
+//                   const SizedBox(height: 20),
+//                   CategoryList(viewModel: viewModel),
+//                   const SizedBox(height: 15),
+//                   Container(
+//                     width: double.infinity,
+//                     height: 10,
+//                     color: const Color(0xFFF2F4F7),
+//                   ),
+//                   const SizedBox(height: 10),
+//                   const TabBar(
+//                     tabs: [
+//                       Tab(text: "Tavsiyalar"),
+//                       Tab(text: "Yozgi savdo"),
+//                     ],
+//                   ),
+//                 ],
+//               ),
+//             ),
+//             SizedBox(
+//               height: 900,
+//               child: TabBarView(
+//                 children: [
+//                   ProductGrid(viewModel: viewModel),
+//                   const Center(
+//                     child: Text("Yozgi savdo content"),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tech_shop/viewmodels/home_view_model.dart';
@@ -41,40 +126,74 @@ class HomeScreen extends StatelessWidget {
             ),
           ],
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Carousel(viewModel: viewModel),
-              const SizedBox(height: 20),
-              CategoryList(viewModel: viewModel),
-              const SizedBox(height: 15),
-              Container(
-                width: double.infinity,
-                height: 10,
-                color: const Color(0xFFF2F4F7),
-              ),
-              const SizedBox(height: 10),
-              const TabBar(
-                tabs: [
-                  Tab(text: "Tavsiyalar"),
-                  Tab(text: "Yozgi savdo"),
-                ],
-              ),
-              SizedBox(
-                height: 900,
-                child: TabBarView(
+        body: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverToBoxAdapter(
+                child: Column(
                   children: [
-                    ProductGrid(viewModel: viewModel),
-                    const Center(
-                      child: Text("Yozgi savdo content"),
+                    Carousel(viewModel: viewModel),
+                    const SizedBox(height: 20),
+                    CategoryList(viewModel: viewModel),
+                    const SizedBox(height: 15),
+                    Container(
+                      width: double.infinity,
+                      height: 10,
+                      color: const Color(0xFFF2F4F7),
                     ),
+                    const SizedBox(height: 10),
                   ],
                 ),
+              ),
+              SliverPersistentHeader(
+                delegate: _SliverAppBarDelegate(
+                  TabBar(
+                    tabs: [
+                      Tab(text: "Tavsiyalar"),
+                      Tab(text: "Yozgi savdo"),
+                    ],
+                  ),
+                ),
+                pinned: true,
+              ),
+            ];
+          },
+          body: TabBarView(
+            children: [
+              ProductGrid(viewModel: viewModel),
+              const Center(
+                child: Text("Yozgi savdo content"),
               ),
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate(this._tabBar);
+
+  final TabBar _tabBar;
+
+  @override
+  double get minExtent => _tabBar.preferredSize.height;
+
+  @override
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      color: Colors.white,
+      child: _tabBar,
+    );
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return false;
   }
 }
--- lib/views/screens/home_screen/widgets/product_widget.dart
+++ lib/views/screens/home_screen/widgets/product_widget.dart
@@ -5,93 +5,89 @@ import 'package:tech_shop/viewmodels/home_view_model.dart';
 class ProductGrid extends StatelessWidget {
   final HomeViewModel viewModel;
 
-  ProductGrid({required this.viewModel});
-  HomeViewModel _homeViewModel = HomeViewModel();
+  ProductGrid({super.key, required this.viewModel});
+
+  final HomeViewModel _homeViewModel = HomeViewModel();
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: _homeViewModel.onCarouselItemTap(),
-        builder: (context, AsyncSnapshot snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (!snapshot.hasData || snapshot.hasError) {
-            return Center(
-              child: Text('error: snapshot'),
-            );
-          } else {
-            List<Product> products = snapshot.data;
-            return GridView.builder(
-              shrinkWrap: true,
-              padding: const EdgeInsets.all(10),
-              physics: const NeverScrollableScrollPhysics(),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-                mainAxisExtent: 400, // Adjust this based on your needs
-                crossAxisCount: 2,
-              ),
-              itemCount: products.length,
-              itemBuilder: (context, index) {
-                final productItem = products[index];
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      clipBehavior: Clip.hardEdge,
-                      height: 200,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Image.network(productItem.images[0],
-                          fit: BoxFit.cover),
+      future: _homeViewModel.onCarouselItemTap(),
+      builder: (context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (!snapshot.hasData || snapshot.hasError) {
+          return const Center(
+            child: Text('error: snapshot'),
+          );
+        } else {
+          List<Product> products = snapshot.data;
+          return GridView.builder(
+            padding: const EdgeInsets.all(10),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              mainAxisExtent: 300, // Adjust this based on your needs
+              crossAxisCount: 2,
+            ),
+            itemCount: products.length,
+            itemBuilder: (context, index) {
+              final productItem = products[index];
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    clipBehavior: Clip.hardEdge,
+                    height: 200,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            productItem.name,
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                            style: const TextStyle(fontSize: 12),
+                    child:
+                        Image.network(productItem.images[0], fit: BoxFit.cover),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          productItem.name,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(fontSize: 12),
+                        ),
+                        const SizedBox(height: 10),
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: const Color(0xFFFFFF00),
                           ),
-                          const SizedBox(height: 10),
-                          Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(5),
-                              color: const Color(0xFFFFFF00),
-                            ),
-                            child: Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 5),
-                              child: Text(productItem.category.toString()),
-                            ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 5),
+                            child: Text(productItem.category.toString()),
                           ),
-                          Text(
-                            productItem.price.toString(),
-                            style: const TextStyle(
-                              decoration: TextDecoration.lineThrough,
-                              decorationThickness: 2,
-                              decorationColor: Colors.grey,
-                              color: Colors.grey,
-                            ),
+                        ),
+                        Text(
+                          productItem.price.toString(),
+                          style: const TextStyle(
+                            decoration: TextDecoration.lineThrough,
+                            decorationThickness: 2,
+                            decorationColor: Colors.grey,
+                            color: Colors.grey,
                           ),
-                          // Text(
-                          //   productItem.discountedPrice,
-                          //   style: const TextStyle(fontWeight: FontWeight.bold),
-                          // ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                  ],
-                );
-              },
-            );
-          }
-        });
+                  ),
+                ],
+              );
+            },
+          );
+        }
+      },
+    );
   }
 }
