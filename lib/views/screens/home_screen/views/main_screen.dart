9b1e09bffea6f9ba60c2612ed42cefb3e15f3b7b
Aabdulloh2005 <ganievabdulloh2005@gmail.com>
2024-06-14 15:05:00 +0500

    search screen

--- lib/views/screens/home_screen/views/main_screen.dart
+++ lib/views/screens/home_screen/views/main_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tech_shop/utils/text_style.dart';
 import 'package:tech_shop/views/screens/home_screen/views/home_screen.dart';
+import 'package:tech_shop/views/screens/home_screen/views/search_screen.dart';
+import 'package:tech_shop/views/screens/home_screen/widgets/public_appbar.dart';
 import 'package:tech_shop/views/screens/home_screen/widgets/custom_tab_box_button.dart';
 
 class MainScreen extends StatefulWidget {
@@ -16,10 +18,42 @@ class _MainScreenState extends State<MainScreen> {
   int _currentIndex = 0;
   final List<Widget> _pages = [
     HomeScreen(),
+    SearchScreen(),
     Placeholder(),
     Placeholder(),
     Placeholder(),
-    Placeholder(),
+  ];
+  final List _appBar = [
+    PublicAppBar(
+        leading: false,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              CupertinoIcons.heart,
+              size: 29,
+            ),
+          ),
+        ],
+        title: const TextField(
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Color(0xFFF3F4F8),
+            label: Text(
+              "Mahsulotlar va turkumlar qidirish",
+              style: TextStyle(
+                color: Color(0xff8B8B95),
+              ),
+            ),
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          ),
+        ),
+        onTap: () {}),
+    PublicAppBar(title: Text("") , onTap: () {}),
+    PublicAppBar(title: Text("Savat"), onTap: () {}),
+    PublicAppBar(title: Text("Saqlangan"), onTap: () {}),
+    PublicAppBar(title: Text("Profil"), onTap: () {})
   ];
 
   void onTabBoxPressed(int index) {
@@ -33,34 +67,7 @@ class _MainScreenState extends State<MainScreen> {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          surfaceTintColor: Colors.white,
-          title: const TextField(
-            decoration: InputDecoration(
-              filled: true,
-              fillColor: Color(0xFFF3F4F8),
-              label: Text(
-                "Mahsulotlar va turkumlar qidirish",
-                style: TextStyle(
-                  color: Color(0xff8B8B95),
-                ),
-              ),
-              border: InputBorder.none,
-              contentPadding:
-                  EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            ),
-          ),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                CupertinoIcons.heart,
-                size: 29,
-              ),
-            ),
-          ],
-        ),
+        appBar: _appBar[_currentIndex],
         body: _pages[_currentIndex],
         bottomNavigationBar: Container(
           height: 50.h,
