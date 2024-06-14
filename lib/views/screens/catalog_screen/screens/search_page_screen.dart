cf48c2e3e411027fb2b8dc78e0e19569a7a2e5bc
Aabdulloh2005 <ganievabdulloh2005@gmail.com>
2024-06-14 18:59:34 +0500

    search ui qilindi

---
+++ lib/views/screens/catalog_screen/screens/search_page_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:tech_shop/viewmodels/home_view_model.dart';
+import 'package:tech_shop/views/screens/home_screen/widgets/category_widget.dart';
+import 'package:tech_shop/views/screens/home_screen/widgets/product_widget.dart';
+
+class SearchPageWidget extends StatelessWidget {
+  HomeViewModel homeViewModel = HomeViewModel();
+  SearchPageWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leadingWidth: 30,
+        title: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: const TextField(
+            autofocus: true,
+            scrollPadding: EdgeInsets.all(10),
+            decoration: InputDecoration(
+              prefixIcon: Icon(
+                Icons.search,
+                size: 20,
+                color: Color(0xff8B8B95),
+              ),
+              filled: true,
+              fillColor: Color(0xffF3F4F8),
+              hintStyle: TextStyle(
+                color: Color(0xff8B8B95),
+              ),
+              hintText: "Termada qidirish",
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 12),
+            ),
+          ),
+        ),
+      ),
+      body: ProductGrid(
+        viewModel: homeViewModel,
+      ),
+    );
+  }
+}
---
+++ lib/views/screens/catalog_screen/widgets/read_textfield_widget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tech_shop/viewmodels/home_view_model.dart';
+import 'package:tech_shop/views/screens/catalog_screen/screens/search_page_screen.dart';
+
+class ReadTextfieldWidget extends StatelessWidget {
+  bool readOnly;
+  String hintText;
+  ReadTextfieldWidget({
+    required this.readOnly,
+    this.hintText = 'Mahsulot va toifalarni qidirish',
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: TextField(
+        readOnly: readOnly,
+        onTap: () => readOnly
+            ? Navigator.of(context).push(
+                CupertinoPageRoute(
+                  builder: (context) => SearchPageWidget(),
+                ),
+              )
+            : null,
+        scrollPadding: const EdgeInsets.all(10),
+        decoration: const InputDecoration(
+          prefixIcon: Icon(
+            Icons.search,
+            size: 20,
+            color: Color(0xff8B8B95),
+          ),
+          filled: true,
+          fillColor: Color(0xffF3F4F8),
+          hintStyle: TextStyle(
+            color: Color(0xff8B8B95),
+          ),
+          hintText: "Mahsulot va toifalarni qidirish",
+          border: InputBorder.none,
+          contentPadding: EdgeInsets.only(top: 12),
+        ),
+      ),
+    );
+  }
+}
