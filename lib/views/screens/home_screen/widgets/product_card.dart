b05c3a1bdc86c58a2b1cd5b8a132dc30edde16e2
TojiddinovMuhammadabdullox <rahmatillooyatillo9@gmail.com>
2024-06-14 16:04:48 +0500

    home screenga sevimli button qoshdim

--- lib/views/screens/home_screen/widgets/product_card.dart
+++ lib/views/screens/home_screen/widgets/product_card.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tech_shop/models/product_item.dart';
 import 'package:tech_shop/utils/routes.dart';
@@ -23,12 +24,27 @@ class ProductCard extends StatelessWidget {
         children: [
           Container(
             clipBehavior: Clip.hardEdge,
-            height: 200,
+            height: 220,
             width: double.infinity,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(
+                image: NetworkImage(
+                  product.images[0],
+                ),
+                fit: BoxFit.cover,
+              ),
             ),
-            child: Image.network(product.images[0], fit: BoxFit.cover),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(CupertinoIcons.heart),
+                )
+              ],
+            ),
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
@@ -39,27 +55,29 @@ class ProductCard extends StatelessWidget {
                   product.name,
                   maxLines: 2,
                   overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(fontSize: 12),
+                  style: const TextStyle(fontSize: 18),
+                ),
+                const Row(
+                  children: [
+                    Icon(Icons.star, color: Color(0XffF59815), size: 15),
+                    Text("7.9", style: TextStyle(color: Colors.grey)),
+                    Text("(11ta baho)")
+                  ],
                 ),
                 const SizedBox(height: 10),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5),
-                    color: const Color(0xFFFFFF00),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 5),
-                    child: Text(product.category.toString()),
-                  ),
-                ),
                 Text(
                   product.price.toString(),
                   style: const TextStyle(
-                    decoration: TextDecoration.lineThrough,
-                    decorationThickness: 2,
-                    decorationColor: Colors.grey,
-                    color: Colors.grey,
-                  ),
+                      decoration: TextDecoration.lineThrough,
+                      decorationThickness: 2,
+                      decorationColor: Colors.grey,
+                      color: Colors.grey,
+                      fontSize: 14),
+                ),
+                Text(
+                  product.category.toString(),
+                  style: const TextStyle(
+                      fontSize: 16, fontWeight: FontWeight.bold),
                 ),
               ],
             ),
